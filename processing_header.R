27aabf9fb9124ef16d554a476edf6c2a8c1b292b
amelotik <amelotik@nd.edu>
2025-11-23 10:34:16 -0500

    Revise header
    Signed-off-by: amelotik <amelotik@nd.edu>

--- processing_header.R
+++ processing_header.R
@@ -4,6 +4,7 @@
 library(collections)
 library(dplyr)
 library(readxl)
+library(openxlsx)
 library(googledrive) #https://googledrive.tidyverse.org/
 library(googlesheets4)
 library(tidyverse)
@@ -13,23 +14,22 @@ read_into_dataframe <- function(raw_data) {
   
   #create column names for data frame
   for (i in 3:7) {
-    colnames(all_data)[i] = raw_data[2, i]
-  }
-  for (i in 8:length(all_data[1,])) {
     colnames(all_data)[i] = raw_data[1, i]
-    #make all areas numeric too, not sure why they aren't automatically numbers
   }
+  #for (i in 8:length(all_data[1,])) {
+    #colnames(all_data)[i] = raw_data[1, i]
+    #make all areas numeric too, not sure why they aren't automatically numbers
+  #}
   
-  #remove first 2 rows of data frame (were just set as column names)
-  all_data <- data.frame(lapply(all_data, function(x) tail(x, -2)))
+  #remove first row of data frame (were just set as column names)
+  all_data <- data.frame(lapply(all_data, function(x) tail(x, -1)))
   
   #make the row names the trial names
   row.names(all_data) <- all_data$Name
   all_data[, 1:3] <- NULL
   
   #force blank levels to 0, and make all levels numeric instead of char
-  all_data <- all_data %>% mutate(Level = if_else(Level == "", 0, as.numeric(Level)))
-  
+  all_data <- all_data %>% mutate(Level = if_else(is.na(Level), 0, as.numeric(Level)))
   
   return (all_data)
 }
